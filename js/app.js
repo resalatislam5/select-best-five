1c8fb683503c51d2460856d57cb711656d1f41e7
resalatislam5 <resalatislam5@gmail.com>
2022-08-22 15:50:35 +0600

    player section js

--- js/app.js
+++ js/app.js
@@ -7,15 +7,19 @@ document.getElementById('btn-Neymar').addEventListener('click', function () {
 
 })
 document.getElementById('btn-Kylian').addEventListener('click', function () {
-    SelectedBtn('btn-Neymar', 'selected-v')
-
-})
-document.getElementById('btn-Neymar').addEventListener('click', function () {
     SelectedBtn('btn-Kylian', 'selected-v')
 
 })
-document.getElementById('btn-Neymar').addEventListener('click', function () {
-    SelectedBtn('btn-Neymar', 'selected-v')
+document.getElementById('btn-Machado').addEventListener('click', function () {
+    SelectedBtn('btn-Machado', 'selected-v')
+
+})
+document.getElementById('btn-Ramos').addEventListener('click', function () {
+    SelectedBtn('btn-Ramos', 'selected-v')
+
+})
+document.getElementById('btn-Renato').addEventListener('click', function () {
+    SelectedBtn('btn-Renato', 'selected-v')
 
 })
 
