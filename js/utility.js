1893d103ba301ea5b032592b0a44188e1102f55a
resalatislam5 <resalatislam5@gmail.com>
2022-08-23 13:45:24 +0600

    common selected player function and inputString to Number

--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,6 @@
+// common selected player function
 function SelectedBtn(btn,select) {
+        const arr = []
         const selectedPlayer = document.getElementById(select);
         const player = document.getElementById(btn);
         const playerName = player.parentNode.parentNode.children[0].innerText;
@@ -6,9 +8,35 @@ function SelectedBtn(btn,select) {
         const playerList = document.createTextNode(playerName);
         li.appendChild(playerList)
         selectedPlayer.appendChild(li);
+        arr.push(selectedPlayer);
+        if( arr[0].children.length > 4){
+            const disables =  document.querySelectorAll('.btn-disable')
+            for (const disable of disables) {
+                disable.disabled = 'true';
+            }
+            alert('ar click korta paban na')
+            player.disabled = 'true';
+            player.style.color = '#fff'
+            player.style.background = '#000';
+            player.style.background = 'red';
+            return;
+        }
         player.disabled = 'true';
         player.style.color = '#fff'
         player.style.background = 'red';
-
+}
+// inputString to Number 
+function cost(insertCost) {
+    const perplayer = document.getElementById(insertCost);
+    const perplayerString = perplayer.value;
+    const perplayerValue = parseFloat(perplayerString);
+    return perplayerValue;
 }
 
+document.getElementById('btn-calculate-player').addEventListener('click', function () {
+    const selectedV = document.getElementById('selected-v');
+    const perplayerValue = cost('per-player')
+    const perplayerResult = selectedV.children.length * perplayerValue;
+    const insertPlayerCost = document.getElementById('insert-player-cost');
+    insertPlayerCost.innerText =  perplayerResult;
+})
